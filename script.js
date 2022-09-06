407702c0005cbee415d13e7ecc5ff4c265fd43fe
pnguyen-81 <109247194+pnguyen-81@users.noreply.github.com>
2022-09-06 15:41:59 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// $("#APPETIZERS").css("visibility","hidden");
+// $("#menuITEMS").css("visibility","hidden");
+// $("#APPETIZER").click(function() {
+//     $("#APPETIZERS").css("visibility", "visible");
+//     $("#menuITEMS").css("visibility","visible");
+//     // $("html").css("filter", "blur(8px)");
+// });
+
+// $("#APPETIZERS").click(function() {
+//     $("#APPETIZERS").css("visibility", "hidden");
+//     $("#menuITEMS").css("visibility","hidden");
+//     // $("html").css("filter", "blur(8px)");
+// });
+
+$("#contact-us-alert").click(function() {
+    alert("Contact Page goes here!");
+});
