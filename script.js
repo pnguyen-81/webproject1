6125b797b9c73103935f0ff56339587eaf1ab3d6
pnguyen-81 <109247194+pnguyen-81@users.noreply.github.com>
2022-09-08 11:28:24 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
-// $("#APPETIZERS").css("visibility","hidden");
-// $("#menuITEMS").css("visibility","hidden");
-// $("#APPETIZER").click(function() {
-//     $("#APPETIZERS").css("visibility", "visible");
-//     $("#menuITEMS").css("visibility","visible");
-//     // $("html").css("filter", "blur(8px)");
-// });
 
-// $("#APPETIZERS").click(function() {
-//     $("#APPETIZERS").css("visibility", "hidden");
-//     $("#menuITEMS").css("visibility","hidden");
-//     // $("html").css("filter", "blur(8px)");
-// });
-
-$("#contact-us-alert").click(function() {
-    alert("Contact Page goes here!");
-});
