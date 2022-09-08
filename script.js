a0d5388de0138b4b78ba59e258ca1d115a48d3c0
pnguyen-81 <109247194+pnguyen-81@users.noreply.github.com>
2022-09-08 13:31:34 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 
+let mybutton = document.getElementById("myBtn");
+
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    $("#myBtn").css("display", "block");
+  } else {
+    $("#myBtn").css("display", "none");
+  }
+}
+
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
